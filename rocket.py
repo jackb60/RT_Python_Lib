d893b6f2f297696f48d95327e1cb036349529727
Jack Bolte <jack.bolte@outlook.com>
2025-10-05 12:52:03 -0400

    progress

--- rocket.py
+++ rocket.py
@@ -1,15 +1,40 @@
 import Serial
 import bleak
 import struct
+from enum import Enum
+
+class state(Enum):
+    GROUND_TESTING = 0
+    PRE_FLIGHT = 1
+    FLIGHT = 2
+    ROLL_CONTROL = 3
+    APOGEE = 3
+    DISREEF = 4
+
 
 class rocket:
     def __init__(self):
+        self.pid = [0, 0, 0] #p, i, d
         self.pyros = [0] * 8
         self.servos = [0] * 8
         self.accelerometer = [0, 0, 0] #x, y, z
         self.barometer = 0
         self.temp = 0
-        self.pid = [0, 0, 0] #p, i, d
+        self.magnetometer = [0, 0, 0] #x, y, z
+        self.gps_fix = False
+        self.lat = 0
+        self.long = 0
+        self.alt = 0
+        self.pdop = 0
+        self.hdop = 0
+        self.vdop = 0
+        self.flight_time = 0
+        self.last_rec = 0
+        self.yaw_gyro_int = 0
+        self.pitch_gyro_int = 0
+        self.roll_gyro_int = 0
+        self.batt_voltage = 0
+        self.state = state.GROUND_TESTING
 
     """
     Returns: False if failed (no data/bad data), True if success
@@ -58,8 +83,37 @@ class rocket:
             self.accelerometer[2] = struct.unpack("<h", packet[18:20])[0]
 
             #Parse barometer
-            self.barometer = struct.unpack("")
+            #self.barometer = struct.unpack("<i",)[0] #TODO
 
+            #Parse temp
+            #self.temp = struct.unpack("<i", )[0] #TODO
+
+            #Parse magnetometer
+            #TODO
+
+            #Parse GPS
+            self.gps_fix = packet[32] & 0x01
+            self.lat = struct.unpack("<f", packet[39:43])[0]
+            self.long = struct.unpack("<f", packet[43:47])[0]
+            self.alt = struct.unpack("<f", packet[47:51])[0]
+            self.pdop = struct.unpack("<f", packet[51:55])[0]
+            self.hdop = struct.unpack("<f", packet[55:59])[0]
+            self.vdop = struct.unpack("<f", packet[59:63])[0]
+
+            #Parse timing
+            self.flight_time = struct.unpack("<i", packet[63:67])[0]
+            self.last_rec = struct.unpack("<i", packet[67:71])[0]
+
+            #Parse Gyro Integrated
+            self.yaw_gyro_int = struct.unpack("<f", packet[71:75])[0]
+            self.pitch_gyro_int = struct.unpack("<f", packet[75:79])[0]
+            self.roll_gyro_int = struct.unpack("<f", packet[79:83])[0]
+
+            #Parse Battery Voltage
+            self.batt_voltage = struct.unpack("<f", packet[83:87])[0]
+
+            #Parse State
+            #TODO
 
     def ground_downlink_update(self):
         pass
